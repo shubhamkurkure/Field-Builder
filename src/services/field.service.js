21caa245120e0a4cc7a8f2d464ed71f1f334b8f5
kurkures96 <kurkure.s@husky.neu.edu>
2021-02-22 00:34:59 -0500

    Added no-cors mode

--- src/services/field.service.js
+++ src/services/field.service.js
@@ -2,6 +2,7 @@ import {API_URL} from "../common/constants";
 
 const fieldService = async (fieldBuilderData) => {
     const response = await fetch(API_URL, {
+        mode: 'no-cors',
         method : 'POST',
         body :JSON.stringify(fieldBuilderData),
         headers : {
